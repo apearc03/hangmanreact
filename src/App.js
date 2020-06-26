41a89ebd0b65dc54592ef7d2cf6b6e9ef24f2388
Alex <arpearce93@gmail.com>
2020-06-26 01:57:03 +0100

    Hangman split

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ class WordLetter extends React.Component {
   render() {
     return (
       <div class="wordLetter">
-        <div class="slot">{this.shouldShowLetter ? this.props.letter : ""}</div>
+        <div class="slot">{this.shouldShowLetter ? this.props.letter : null}</div>
         <Line angle="0" x="0" y="0" width="70" height="5" canvasWidth="70" canvasHeight="10"></Line>
       </div>
     )
@@ -83,6 +83,7 @@ class App extends React.Component {
     const wordLetters = [];
     const lettersToPick = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     const letterInstances = [];
+    const hangManParts = [];
 
     for (let value of lettersToPick) {
       letterInstances.push(<Letter letter={value} />);
@@ -91,6 +92,17 @@ class App extends React.Component {
     for(let value of this.word){
       wordLetters.push(<WordLetter letter={value}/>);
     }
+
+    hangManParts.push(<Line angle="0" x="0" y="0" width="100" height="20" canvasWidth="100" canvasHeight="20" />);
+    hangManParts.push(<Line angle="90" x="0" y="-20" width="200" height="20" canvasWidth="20" canvasHeight="200" />);
+    hangManParts.push(<Line angle="0" x="0" y="0" width="200" height="20" canvasWidth="200" canvasHeight="20" />);
+    hangManParts.push(<Line angle="90" x="0" y="-156" width="30" height="12" canvasWidth="200" canvasHeight="30" />);
+    hangManParts.push(<Circle x="150" y="25" radius="22" startAngle="0" canvasWidth="200" canvasHeight="50" />);
+    hangManParts.push(<Line angle="90" x="0" y="-20" width="30" height="20" canvasWidth="20" canvasHeight="35" />);
+    hangManParts.push(<Line angle="300" x="31" y="110" width="30" height="15" canvasWidth="140" canvasHeight="35" />);
+    hangManParts.push(<Line angle="55" x="10" y="-10" width="30" height="15" canvasWidth="50" canvasHeight="35" />);
+    hangManParts.push(<Line angle="120" x="-70" y="-130" width="50" height="10" canvasWidth="150" canvasHeight="70" />);
+    hangManParts.push(<Line angle="60" x="5" y="-10" width="50" height="10" canvasWidth="50" canvasHeight="70" />);
 
     return (
       <div id = "app">
@@ -101,26 +113,25 @@ class App extends React.Component {
           {letterInstances}
         </div>
         <div id="hangman">
-          <Line angle="0" x="0" y="0" width="200" height="20" canvasWidth="200" canvasHeight="20" />
+          {hangManParts[2]}
           <div>
-            <Line angle="90" x="0" y="-20" width="200" height="20" canvasWidth="20" canvasHeight="200" />
-
+          {hangManParts[1]}
             <div class="hangmanPart">
-              <div><Line angle="90" x="0" y="-156" width="30" height="12" canvasWidth="200" canvasHeight="30" /></div>
-              <div><Circle x="150" y="25" radius="22" startAngle="0" canvasWidth="200" canvasHeight="50" /></div>
+              <div>{hangManParts[3]}</div>
+              <div>{hangManParts[4]}</div>
               <div >
-                <div class="hangmanPart"><Line angle="300" x="31" y="110" width="30" height="15" canvasWidth="140" canvasHeight="35" /></div>
-                <div class="hangmanPart"><Line angle="90" x="0" y="-20" width="30" height="20" canvasWidth="20" canvasHeight="35" /></div>
-                <div class="hangmanPart"><Line angle="55" x="10" y="-10" width="30" height="15" canvasWidth="50" canvasHeight="35" /></div>
+                <div class="hangmanPart">{hangManParts[6]}</div>
+                <div class="hangmanPart">{hangManParts[5]}</div>
+                <div class="hangmanPart">{hangManParts[7]}</div>
               </div>
               <div>
-                <div class="hangmanPart"><Line angle="120" x="-70" y="-130" width="50" height="10" canvasWidth="150" canvasHeight="70" /></div>
-                <div class="hangmanPart"><Line angle="60" x="5" y="-10" width="50" height="10" canvasWidth="50" canvasHeight="70" /></div>
+                <div class="hangmanPart">{hangManParts[8]}</div>
+                <div class="hangmanPart">{hangManParts[9]}</div>
               </div>
             </div>
 
           </div>
-          <Line angle="0" x="0" y="0" width="100" height="20" canvasWidth="100" canvasHeight="20" />
+          {hangManParts[0]}
         </div>
       </div>
     );
